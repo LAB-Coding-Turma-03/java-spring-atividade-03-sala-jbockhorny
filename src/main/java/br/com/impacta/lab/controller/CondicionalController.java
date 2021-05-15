9ebe5dbbd598310de7eb6fd7a08ae7e2ea542728
joanilza <joanilza.bockhorny@avande.com>
2021-05-15 12:42:05 -0300

    feat: exercício com if

--- src/main/java/br/com/impacta/lab/controller/CondicionalController.java
+++ src/main/java/br/com/impacta/lab/controller/CondicionalController.java
@@ -11,22 +11,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class CondicionalController {
 
 	@GetMapping("/condicional")
-	public ResponseEntity<String> simularValores(@RequestParam(name="idade") int idade) {
+	public ResponseEntity<String> simularValores(@RequestParam(name = "idade") int idade) {
 		/*
-		 * Elabore um algoritmo para dizer a partir da idade se a 
-		 * pessoa tem mais ou menos do que 18 anos.
-		 *  
-		 *  Retornar os seguintes textos:
-		 *  
-		 *  
-		 *  Para maiores de dezoito anos -> Possui mais de 18 anos
-		 *  
-		 *  Para menores de dezoito anos -> Possui menos de 18 anos
-		 *  
+		 * Elabore um algoritmo para dizer a partir da idade se a pessoa tem mais ou
+		 * menos do que 18 anos.
+		 * 
+		 * Retornar os seguintes textos:
+		 * 
+		 * 
+		 * Para maiores de dezoito anos -> Possui mais de 18 anos
+		 * 
+		 * Para menores de dezoito anos -> Possui menos de 18 anos
+		 * 
 		 */
-		
-		
-		return ResponseEntity.ok("APÓS O ALGORITMO COLOCAR O TEXTO/VARIAVEL DE RETORNO AQUI");
+
+		String returnIdade = "Coloque sua idade";
+
+		if (idade == 18) {
+			returnIdade = "Você tem " + idade + " anos.";
+		} else if (idade > 18) {
+			returnIdade = "Você tem " + idade + " anos. Possui mais de 18 anos";
+		} else {
+			returnIdade = "Você tem " + idade + " anos. Possui menos de 18 anos";
+		}
+
+		return ResponseEntity.ok(returnIdade);
 	}
-	
+
 }
